aafff12e476360ce4de65da707cec9616ea0c7cb
ColtMay <colten_mayberry@pm.me>
2021-07-14 13:53:13 -0500

    user can now add tags to tag list

--- Tabloid/Controllers/TagController.cs
+++ Tabloid/Controllers/TagController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tabloid.Models;
 using Tabloid.Repositories;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -36,8 +37,10 @@ namespace Tabloid.Controllers
 
         // POST api/<TagController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post(Tag tag)
         {
+            _tagRepository.Add(tag);
+            return CreatedAtAction("Get", new { id = tag.Id }, tag);
         }
 
         // PUT api/<TagController>/5
