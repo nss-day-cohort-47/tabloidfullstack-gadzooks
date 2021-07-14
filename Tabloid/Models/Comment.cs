3b58da2a01b23d076a1c3ac7316d5f6dc207da8d
Autumn Fotopoulos <acarte94@gmail.com>
2021-07-14 13:35:47 -0500

    working on comment repo

---
+++ Tabloid/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+   
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+    }
+}
